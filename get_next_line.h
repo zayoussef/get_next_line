7454266bdd0f981f00e935e74b086415b2d53101
zayoussef <136023225+zayoussef@users.noreply.github.com>
2023-11-29 19:02:20 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,3 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*get_next_line(int fd);
+char	*ft_get_line(int fd, char *handle);
+char	*ft_handle(char *backup);
+char	*ft_line(char *handle);
+
+#endif
