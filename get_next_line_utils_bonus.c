0ec6908482c41b91f47bdd3a66101f75a7b713a1
zayoussef <136023225+zayoussef@users.noreply.github.com>
2023-11-29 19:03:50 +0100

    Update get_next_line_utils_bonus.c

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,3 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{	
+	if (!s)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return (s);
+		s++;
+	}
+	if (*s == (char)c)
+		return (s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char			*str;
+	size_t			tlen;
+	size_t			i;
+	size_t			j;
+
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	tlen = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(tlen * sizeof(char) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	while (s1[++i] != '\0')
+		str[i] = s1[i];
+	j = 0;
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
