1887aea7890269b82cbe9907f2aff920074587a2
yozainan <yozainan@e2r2p8.1337.ma>
2023-12-02 14:37:41 +0100

    v_finish

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,34 +6,34 @@
 /*   By: yozainan <yozainan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 09:43:45 by yozainan          #+#    #+#             */
-/*   Updated: 2023/12/01 19:59:33 by yozainan         ###   ########.fr       */
+/*   Updated: 2023/12/01 21:24:46 by yozainan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-char	*ft_line(char *handle)
+char	*get_new_line(char *tmp)
 {
 	int		i;
 	char	*str;
 
 	i = 0;
-	if (!handle[i])
+	if (!tmp[i])
 		return (NULL);
-	while (handle[i] && handle[i] != '\n')
+	while (tmp[i] && tmp[i] != '\n')
 		i++;
 	str = malloc(sizeof(char) * (i + 2));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (handle[i] && handle[i] != '\n')
+	while (tmp[i] && tmp[i] != '\n')
 	{
-		str[i] = handle[i];
+		str[i] = tmp[i];
 		i++;
 	}
-	if (handle[i] == '\n')
+	if (tmp[i] == '\n')
 	{
-		str[i] = handle[i];
+		str[i] = tmp[i];
 		i++;
 	}
 	str[i] = '\0';
@@ -84,9 +84,7 @@ char	*ft_get_line(int fd, char *handle)
 		{
 			free(buffer);
 			free(handle);
-			if (!handle)
-				return (NULL);
-			return (handle);
+			return (NULL);
 		}
 		buffer[rbyts] = '\0';
 		handle = ft_strjoin(handle, buffer);
@@ -98,14 +96,14 @@ char	*ft_get_line(int fd, char *handle)
 char	*get_next_line(int fd)
 {
 	char		*buff;
-	static char	*handle[4096];
+	static char	*handle[OPEN_MAX];
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 4096)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > OPEN_MAX)
 		return (NULL);
 	handle[fd] = ft_get_line(fd, handle[fd]);
 	if (!handle[fd])
 		return (NULL);
-	buff = ft_line(handle[fd]);
+	buff = get_new_line(handle[fd]);
 	handle[fd] = ft_handle(handle[fd]);
 	return (buff);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: yozainan <yozainan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 09:43:45 by yozainan          #+#    #+#             */
-/*   Updated: 2023/12/01 18:26:03 by yozainan         ###   ########.fr       */
+/*   Updated: 2023/12/01 21:06:47 by yozainan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
 char	*read_file(int fd, char *handle);
 char	*ft_handle(char *backup);
-char	*get_new_line(char *handle);
+char	*get_new_line(char *tmp);
 
 #endif
